b1a8efacc6d5c6196124aa96d64a183567f25a95
SambaranRepo <sambaranghosal@gmail.com>
2022-05-25 18:43:54 -0700

    Working CEC to track the path

---
+++ controller.py
@@ -0,0 +1,51 @@
+import numpy as np
+from casadi import *
+
+
+def casadi_controller(cur_state, ref):
+     Q = 10*np.eye(2)
+     q = 10
+     R = 5*np.eye(2)
+     cur_ref = ref[0]
+     error_x = 1 * (cur_state[0] - cur_ref[0])
+     error_y = 1 * (cur_state[1] - cur_ref[1])
+     error_theta = -cur_ref[2] + cur_state[2]
+     error_theta = (error_theta + np.pi) % (2 * np.pi) - np.pi
+     opti = casadi.Opti()
+     N = 15
+     U = opti.variable(2,N)
+     E = opti.variable(3,N+1)
+     error = opti.parameter(3,1)
+     opti.set_value(error, vcat([error_x, error_y, error_theta]))
+     v = U[0,:]
+     w = U[1,:]
+     
+
+     obj = 0
+     #For loop to set objective function
+     for i in range(N):
+          obj += (E[:2,i].T @ Q @ E[:2,i] + q * (1 - cos(E[2,i]))**2 + U[:,i].T @ R @ U[:,i])
+     obj += E[:,-1].T @ E[:,-1]
+     opti.minimize(obj)
+     opti.subject_to(opti.bounded(0, U[0,:], 1))
+     opti.subject_to(opti.bounded(-1, U[1,:], 1))
+     opti.subject_to(E[:,0] == error)
+     for i in range(1,N+1):
+          opti.subject_to(E[:,i] == E[:,i-1] + vertcat(hcat([0.5 * cos(E[2, i-1] + ref[i-1][2]), 0]), hcat([0.5 * sin(E[2,i-1] + ref[i-1][2]), 0]), 
+          hcat([0, 0.5])) @ U[:,i-1] \
+               - vcat([ref[i][0] - ref[i-1][0], ref[i][1] - ref[i-1][1], (ref[i][2] - ref[i-1][2] + np.pi) % (2 * np.pi) - np.pi]))
+          opti.subject_to(opti.bounded(vcat([-2.5,-2.5]), E[:2, i] + vcat([ref[i][0], ref[i][1]]), vcat([2.5,2.5])))
+          opti.subject_to((E[0,i] + + ref[i][0] +  2)**2 + (E[1,i]+ ref[i][1] + 2)**2 > 0.5**2)
+          opti.subject_to((E[0,i] + ref[i][0]- 1)**2 + (E[1,i] + ref[i][1] - 2)**2 > 0.5**2)
+     
+
+     
+     opts = {'ipopt.print_level': 0, 'print_time': 0, 'ipopt.sb': 'yes'}
+     opti.solver('ipopt', opts)
+     sol = opti.solve()
+     return sol.value(U)[:,0]
+
+
+#Add the freespace constraint for the position 
+#Implement exact error dynamics by passing the reference trajectory
+#How to generalise for time horizon T > 3
---
+++ main.py
@@ -0,0 +1,126 @@
+from time import time
+import numpy as np
+from utils import visualize
+from controller import *
+
+# Simulation params
+np.random.seed(10)
+time_step = 0.5 # time between steps in seconds
+sim_time = 120    # simulation time
+
+# Car params
+x_init = 1.5
+y_init = 0.0
+theta_init = np.pi/2
+v_max = 1
+v_min = 0
+w_max = 1
+w_min = -1
+
+# This function returns the reference point at time step k
+def lissajous(k):
+    xref_start = 0
+    yref_start = 0
+    A = 2
+    B = 2
+    a = 2*np.pi/50
+    b = 3*a
+    T = np.round(2*np.pi/(a*time_step))
+    k = k % T
+    delta = np.pi/2
+    xref = xref_start + A*np.sin(a*k*time_step + delta)
+    yref = yref_start + B*np.sin(b*k*time_step)
+    v = [A*a*np.cos(a*k*time_step + delta), B*b*np.cos(b*k*time_step)]
+    thetaref = np.arctan2(v[1], v[0])
+    return [xref, yref, thetaref]
+
+# This function implements a simple P controller
+def simple_controller(cur_state, ref_state):
+    k_v = 0.55
+    k_w = 1.0
+    v = k_v*np.sqrt((cur_state[0] - ref_state[0])**2 + (cur_state[1] - ref_state[1])**2)
+    v = np.clip(v, v_min, v_max)
+    angle_diff = ref_state[2] - cur_state[2]
+    angle_diff = (angle_diff + np.pi) % (2 * np.pi ) - np.pi
+    w = k_w*angle_diff
+    w = np.clip(w, w_min, w_max)
+    return [v,w]
+
+# This function implement the car dynamics
+def car_next_state(time_step, cur_state, control, noise = True):
+    theta = cur_state[2]
+    rot_3d_z = np.array([[np.cos(theta), 0], [np.sin(theta), 0], [0, 1]])
+    f = rot_3d_z @ control
+    mu, sigma = 0, 0.04 # mean and standard deviation for (x,y)
+    w_xy = np.random.normal(mu, sigma, 2)
+    mu, sigma = 0, 0.004  # mean and standard deviation for theta
+    w_theta = np.random.normal(mu, sigma, 1)
+    w = np.concatenate((w_xy, w_theta))
+    if noise:
+        return cur_state + time_step*f.flatten() + w
+    else:
+        return cur_state + time_step*f.flatten()
+
+if __name__ == '__main__':
+    # Obstacles in the environment
+    obstacles = np.array([[-2,-2,0.5], [1,2,0.5]])
+    # Params
+    traj = lissajous
+    ref_traj = []
+    error = 0.0
+    car_states = []
+    times = []
+    # Start main loop
+    main_loop = time()  # return time in sec
+    # Initialize state
+    cur_state = np.array([x_init, y_init, theta_init])
+    cur_iter = 0
+    # Main loop
+    while (cur_iter * time_step < sim_time):
+        t1 = time()
+        # Get reference state
+        cur_time = cur_iter*time_step
+        # cur_ref = traj(cur_iter)
+        cur_ref = []
+        for i in range(120):
+            cur_ref.append(traj(cur_iter + i))
+        # Save current state and reference state for visualization
+        ref_traj.append(cur_ref[0])
+        car_states.append(cur_state)
+
+        ################################################################
+        # Generate control input
+        # TODO: Replace this simple controller with your own controller
+        # control = simple_controller(cur_state, cur_ref[0])
+        control = casadi_controller(cur_state, cur_ref)
+        v = control[0]
+        w = control[1]
+        v = np.clip(v, v_min, v_max)
+        w = np.clip(w,w_min, w_max)
+        print("[v,w]", control)
+        ################################################################
+
+        # Apply control input
+        next_state = car_next_state(time_step, cur_state, [v,w], noise=True)
+        # Update current state
+        cur_state = next_state
+        # Loop time
+        t2 = time()
+        print(cur_iter)
+        print(t2-t1)
+        times.append(t2-t1)
+        error = error + np.linalg.norm(cur_state - cur_ref)
+        cur_iter = cur_iter + 1
+
+    main_loop_time = time()
+    print('\n\n')
+    print('Total time: ', main_loop_time - main_loop)
+    print('Average iteration time: ', np.array(times).mean() * 1000, 'ms')
+    print('Final error: ', error)
+
+    # Visualization
+    ref_traj = np.array(ref_traj)
+    car_states = np.array(car_states)
+    times = np.array(times)
+    visualize(car_states, ref_traj, obstacles, times, time_step, save=True)
+
